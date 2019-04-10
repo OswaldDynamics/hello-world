6eb5cf96f855909e586135293b2c16febee59255
Oswald <49497022+OswaldDynamics@users.noreply.github.com>
2019-04-10 21:17:58 +0100

    Added more inputs
    Added more user inputs, and their respective conversational outputs.

---
+++ first-test.py
@@ -0,0 +1,9 @@
+f_name = input("Please enter your first name./n")
+s_name = input("Please enter your surname./n")
+
+print("Hello " + f_name + " " + s_name + ", how are you today?")
+
+mood = input("I hope you are well. Let me know how you are feeling./n")
+fave_colour = input("Well even still, I hope things get better. What is your favorite colour?/n")
+
+print(fave_colour + "is one of my favorites too. Baby poop green wins by a stretch though.")
